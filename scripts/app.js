81867e442417283fac5634a45771562ce624e933
Zahid Afsar <zahidtempz@icloud.com>
2023-12-07 16:23:17 -0800

    thursday Push

--- scripts/app.js
+++ scripts/app.js
@@ -1,115 +1,154 @@
-import {apiKey} from "./environment.js";
+import { apiKey } from "./environment.js";
 import { cityName } from "./environment.js";
 
-async function ApiCall(city) {
-    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
-    const data = await promise.json();
+async function CurrentWeatherApi(city) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
+        const data = await response.json();
 
-    document.getElementById('cityName').innerText = city;
+        document.getElementById('cityName').innerText = city;
+
+        console.log('------Current Weather------', data);
+
+        const currentTemperature = data.main.temp;
+        const maxTemperature = data.main.temp_max;
+        const minTemperature = data.main.temp_min;
+
+        console.log('Current Temperature:', currentTemperature);
+        console.log('Max Temperature:', maxTemperature);
+        console.log('Min Temperature:', minTemperature);
+
+        document.getElementById('currentTemperature').textContent = `${currentTemperature} °F`;
+        document.getElementById('maxTemperature').innerText = `H: ${maxTemperature}°F`;
+        document.getElementById('minTemperature').innerText = `L: ${minTemperature}°F`;
+
+        getLatLonForCity(city);
+    } catch (error) {
+        console.error('Error fetching current weather data:', error);
+    }
+}
 
 
-    console.log('------Current Weather------');
-    const currentTemperature = data.main.temp;
-    const maxTemperature = data.main.temp_max;
-    const minTemperature = data.main.temp_min;
+async function fiveDayApi(lat, lon) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`);
+        const forecastData = await response.json();
 
-    console.log('Current Temperature:', currentTemperature);
-    console.log('Max Temperature:', maxTemperature);
-    console.log('Min Temperature:', minTemperature);
+        console.log('------5 day forecast------');
+        console.log('Data:', forecastData);
 
-    document.getElementById('currentTemperature').textContent = `${currentTemperature} °F`;
-    document.getElementById('maxTemperature').innerText = `H: ${maxTemperature}°F`;
-    document.getElementById('minTemperature').innerText = `L: ${minTemperature}°F`;
+        const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        const currentDate = new Date();
+
+        for (let i = 0; i < 5; i++) {
+            const index = Math.floor((i * 8) + (currentDate.getHours() / 3));
+
+            if (index < forecastData.list.length) {
+                let TemperatureMin = forecastData.list[index].main.temp_min;
+                let TemperatureMax = forecastData.list[index].main.temp_max;
+
+                const dayElement = document.getElementById(`day${i}`);
+                if (dayElement) {
+                    dayElement.innerHTML = `${dayNames[(currentDate.getDay() + i) % 7]}<br>H: ${TemperatureMax}°F<br>L: ${TemperatureMin}°F`;
+                }
+            } else {
+                console.error(`Invalid index ${index} for the list array`);
+            }
+        }
+
+        console.log('------5 day forecast------');
+        console.log(forecastData);
+    } catch (error) {
+        console.error('Error fetching forecast data:', error);
+    }
 }
 
 window.onload = function () {
-    ApiCall('Los Angeles')
+    const addToFavoritesBtn = document.getElementById('addToFavoritesBtn');
+    const SearchBar = document.getElementById("SearchBar");
+    const SubmitBtn = document.getElementById("SubmitBtn");
 
-}
+    SearchBar.addEventListener('click', function (e) {
+        console.log(SearchBar.value);
+    });
 
-async function ApiCall2(){
+    SubmitBtn.addEventListener('click', function (e) {
+        SearchCall();
+    });
 
-    const promise = await  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=37.9577&lon=-121.275604&appid=${apiKey}&units=imperial`);
+    addToFavoritesBtn.addEventListener('click', function (e) {
+        const searchInput = document.getElementById('cityName').innerText; 
+        if (searchInput.trim() !== '') {
+            addToFavorites(searchInput);
+            alert('added to favorites');
+            updateFavoritesPageNew(JSON.parse(localStorage.getItem('favorites')) || []);
+        }
+    });
 
-    const data = await promise.json();
+    CurrentWeatherApi('Los Angeles');
+    fiveDayApi(34.0522, -118.2437); 
+    updateDateTime();
+    setInterval(updateDateTime, 60000);
+};
 
-    console.log('------5 day forecast------')
-
-    let currentTemperatureMin = data.list[0].main.temp_min;
-    let currentTemperatureMax = data.list[0].main.temp_max;
-    console.log('Min tommorow', currentTemperatureMin);
-    console.log('Max tommorrow', currentTemperatureMax);
-
-    document.getElementById('currentTemperatureMax').innerText = `H: ${currentTemperatureMax}°F`;
-    document.getElementById('currentTemperatureMin').innerText = `L: ${currentTemperatureMin}°F`;
-
-    console.log('------5 day forecast------')
-
-    let TemperatureMin = data.list[8].main.temp_min;
-    let TemperatureMax = data.list[8].main.temp_max;
-    console.log('Min next day', TemperatureMin);
-    console.log('Max next day', TemperatureMax);
-
-    document.getElementById('TemperatureMax').innerText = `H: ${TemperatureMax}°F`;
-    document.getElementById('TemperatureMin').innerText = `L: ${TemperatureMin}°F`;
-
-    console.log('------5 day forecast------')
-
-    let TemperatureMin2 = data.list[16].main.temp_min;
-    let TemperatureMax2 = data.list[16].main.temp_max;
-    console.log('Min next day 2', TemperatureMin2);
-    console.log('Max next day 2', TemperatureMax2);
-
-    document.getElementById('TemperatureMax2').innerText = `H: ${TemperatureMax2}°F`;
-    document.getElementById('TemperatureMin2').innerText = `L: ${TemperatureMin2}°F`;
-
-    console.log('------5 day forecast------')
-
-    let TemperatureMin3 = data.list[32].main.temp_min;
-    let TemperatureMax3 = data.list[32].main.temp_max;
-    console.log('Min next day 3', TemperatureMin3);
-    console.log('Max next day 3', TemperatureMax3);
-
-    document.getElementById('TemperatureMax3').innerText = `H: ${TemperatureMax3}°F`;
-    document.getElementById('TemperatureMin3').innerText = `L: ${TemperatureMin3}°F`;
-
-    console.log(data)
-
-}
-
-async function SearchCall(){
+async function SearchCall() {
     const searchInput = document.getElementById('SearchBar').value;
-    await ApiCall(searchInput);
+    if (searchInput.trim() !== '') {
+        await CurrentWeatherApi(searchInput);
+        getLatLonForCity(searchInput);
+    }
 }
 
-async function timeDate(){
+async function getLatLonForCity(city) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+        const data = await response.json();
 
-    const promise = await  fetch();
+        const latitude = data.coord.lat;
+        const longitude = data.coord.lon;
 
-    const data = await promise.json();
-
-    console.log(data)
+        if (latitude && longitude) {
+            fiveDayApi(latitude, longitude);
+        } else {
+            console.error('Invalid latitude or longitude values:', data.coord);
+        }
+    } catch (error) {
+        console.error('Error fetching latitude and longitude data:', error);
+    }
 }
 
 
 
-ApiCall(cityName);
-ApiCall2();
-SearchCall();
-timeDate();
+function addToFavorites(city) {
+    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
+    if (!favorites.includes(city)) {
+        favorites.push(city);
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+        updateFavoritesPage(favorites);
+        console.log(`Added ${city} to favorites.`);
+    } else {
+        console.log(`${city} is already in favorites.`);
+    }
+}
 
-let SearchBar = document.getElementById("SearchBar");
-let SubmitBtn = document.getElementById("SubmitBtn");
+function updateFavoritesPage(favorites) {
+    console.log('Updated favorites:', favorites);
+}
 
-SearchBar.addEventListener('click', function(e){
-    console.log(SearchBar.value);
-});
+function updateDateTime() {
+    const currentDate = new Date();
 
+    const formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
-SubmitBtn.addEventListener('click', function(e){
-    SearchCall();
-});
+    const options = { weekday: 'long', day: 'numeric', month: 'short' };
+    const formattedDate = currentDate.toLocaleDateString([], options);
+
+    document.getElementById('textTime').textContent = formattedTime;
+    document.getElementById('dateTime').textContent = formattedDate;
+}
+
 
 
 // THIS CODE WOULDNT WORK IN JS BUT WORKS IN HTML FOR SOME REASON????
